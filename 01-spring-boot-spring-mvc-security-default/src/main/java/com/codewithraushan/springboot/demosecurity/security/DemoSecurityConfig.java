6df5a60f28cc3ee76831bb8d7668ed71d146a66b
Raushanray <raushankuray1998@gmail.com>
2023-07-06 12:31:54 +0530

    create a custom login page

--- 01-spring-boot-spring-mvc-security-default/src/main/java/com/codewithraushan/springboot/demosecurity/security/DemoSecurityConfig.java
+++ 01-spring-boot-spring-mvc-security-default/src/main/java/com/codewithraushan/springboot/demosecurity/security/DemoSecurityConfig.java
@@ -2,9 +2,11 @@ package com.codewithraushan.springboot.demosecurity.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class DemoSecurityConfig {
@@ -31,5 +33,21 @@ public class DemoSecurityConfig {
 
         return new InMemoryUserDetailsManager(ravi,raushan,sumit);
 
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+
+        http.authorizeHttpRequests(configure ->
+                configure
+                        .anyRequest().authenticated()
+                )
+                .formLogin(form ->
+                          form
+                                  .loginPage("/showMyLoginPage")
+                                  .loginProcessingUrl("/authenticateTheUser")
+                                  .permitAll()
+                        );
+        return http.build();
     }
 }
